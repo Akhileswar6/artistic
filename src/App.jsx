c4684212008e02bf4b02da9edec648248c0a7b53
Akhileswar Kamale <akhilkamale@gmail.com>
2026-03-16 22:45:10 +0530

    Update main page

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Gallery from "./Pages/Gallery";
 import Order from "./Pages/Order";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
+import CustomerShowcase from "./Pages/CustomerShowcase";
 
 // Policy Pages
 import Terms from "./Pages/Policies/TermsOfService";
@@ -53,6 +54,7 @@ export default function App() {
           <Route path="/order" element={<Order isDark={isDark} />} />
           <Route path="/about" element={<About isDark={isDark} />} />
           <Route path="/contact" element={<Contact isDark={isDark} />} />
+          <Route path="/showcase" element={<CustomerShowcase isDark={isDark} />} />
         </Route>
 
         {/* ========================= */}
