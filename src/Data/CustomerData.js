31c4fcf005e6903008308a1780d7a8f2cd401172
Akhileswar Kamale <akhilkamale@gmail.com>
2026-03-17 17:00:50 +0530

    feat: Add data of customers

---
+++ src/Data/CustomerData.js
@@ -0,0 +1,53 @@
+export const customers = [
+  {
+    id: 1,
+    name: "Rahul",
+    style: "Charcoal",
+    image: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e",
+    review: "Amazing artwork!"
+  },
+  {
+    id: 2,
+    name: "Priya",
+    style: "Pencil",
+    image: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e",
+    review: "Beautiful drawing"
+  },
+  {
+    id: 3,
+    name: "Arjun",
+    style: "Color",
+    image: "https://images.unsplash.com/photo-1544725176-7c40e5a71c5e",
+    review: "Loved it"
+  },
+  {
+    id: 4,
+    name: "Sneha",
+    style: "Charcoal",
+    image: "https://images.unsplash.com/photo-1520813792240-56fc4a3765a7",
+    review: "Super realistic"
+  },
+  {
+    id: 5,
+    name: "Kiran",
+    style: "Pencil",
+    image: "https://images.unsplash.com/photo-1494790108377-be9c29b29330",
+    review: "Awesome"
+  },
+  {
+    id: 6,
+    name: "Anjali",
+    style: "Color",
+    image: "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91",
+    review: "Highly recommend"
+  },
+  {
+    id: 7,
+    name: "Vikram",
+    style: "Charcoal",
+    image: "https://images.unsplash.com/photo-1517841905240-472988babdf9",
+    review: "Incredible detail"
+  }
+  
+  
+];
