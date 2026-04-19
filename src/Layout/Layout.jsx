8eee17be229f9d728264e6b34a0d155c03b0e5de
Akhileswar Kamale <akhilkamale@gmail.com>
2026-04-19 23:26:03 +0530

    feat: updated theme layout

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-export default function Layout({ isDark, setIsDark, setUser }) {
+export default function Layout({ isDark, setIsDark, user, setUser }) {
   return (
     <div
       className={`min-h-screen flex flex-col transition-colors duration-300 ${
@@ -16,7 +16,7 @@ export default function Layout({ isDark, setIsDark, setUser }) {
 
       {/* Main Content */}
       <main className="flex-grow">
-        <Outlet />
+        <Outlet context={{ user, isDark, setIsDark, setUser }} />
       </main>
 
       {/* Footer */}
