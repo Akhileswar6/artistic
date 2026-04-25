56f563b66b2dd0015fc4ad8aff7d1ca765720262
Akhileswar Kamale <akhilkamale@gmail.com>
2026-04-25 13:41:13 +0530

    feat: Added layout ui

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -5,14 +5,13 @@ import Footer from "./Footer";
 export default function Layout({ isDark, setIsDark, user, setUser }) {
   return (
     <div
-      className={`min-h-screen flex flex-col transition-colors duration-300 ${
-        isDark
+      className={`min-h-screen flex flex-col transition-colors duration-300 ${isDark
           ? "bg-[#0f1115] text-white"
           : "bg-white text-black"
-      }`}
+        }`}
     >
       {/* Navbar */}
-      <Navbar isDark={isDark} setIsDark={setIsDark} setUser={setUser} />
+      <Navbar isDark={isDark} setIsDark={setIsDark} user={user} setUser={setUser} />
 
       {/* Main Content */}
       <main className="flex-grow">
